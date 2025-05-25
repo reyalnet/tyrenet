d439b7c1f425cc1a36c5119522e8a4ae7bae4bfa
Patrick Willy <patrick@reyal.biz>
2025-05-25 22:57:06 +0100

    New Tool - Car Inspection

---
+++ tyrenet/tyrenet/doctype/car_inspection/car_inspection.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Cubenet and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CarInspection(Document):
+	pass
---
+++ tyrenet/tyrenet/doctype/car_inspection/test_car_inspection.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Cubenet and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCarInspection(FrappeTestCase):
+	pass
